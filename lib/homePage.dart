a6f0bbd2a1a76a01bbc67fa8136200117c11bd3b
Sourov <sourovchandra921@gmail.com>
2025-10-17 01:24:09 +0600

    first page done

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -18,48 +18,57 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          SizedBox(height: 50),
-          Center(child: Image.asset("Assert/profile.png")),
-          SizedBox(height: 30),
-          Text(
-            "Why Choose Us",
-            style: TextStyle(
-              fontSize: 25,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 10,),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              "We make parking easy, secure, and stress-free. Whether you're driving a car or a truck, find and reserve reliable parking spots in real-time, wherever you are. With our simple booking and payment system, your parking experience is just a tap away!",
-              style: TextStyle(color: Colors.white,fontSize: 15,letterSpacing: 0.8),
-            ),
-          ),
-          SizedBox(height: 200,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Icon(Icons.abc_rounded,color: Colors.red,size: 30,),
 
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal      : 8),
-                child: Container(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 40),
+        child: Column(
+           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+              Center(child: Image.asset("Assert/profile.png")),
+                SizedBox(
                   height: 40,
-                  width: 100,
-                  color: Colors.red,
-                  child: Center(
-                    child: Text("Next",style: TextStyle(color: Colors.white),),
+                ),
+                Text(
+                  "Why Choose Us",
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              )
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "We make parking easy, secure, and stress-free. Whether you're driving a car or a truck, find and reserve reliable parking spots in real-time, wherever you are. With our simple booking and payment system, your parking experience is just a tap away!",
+                    style: TextStyle(color: Colors.white,fontSize: 15,letterSpacing: 0.8),
+                  ),
+                ),
+              ],
+            ),
 
-            ],
-          )
-        ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(Icons.abc_rounded,color: Colors.red,size: 30,),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal      : 8),
+                  child: Container(
+                    height: 40,
+                    width: 100,
+                    color: Colors.red,
+                    child: Center(
+                      child: Text("Next",style: TextStyle(color: Colors.white),),
+                    ),
+                  ),
+                )
+
+              ],
+            )
+          ],
+        ),
       ),
 
     );
